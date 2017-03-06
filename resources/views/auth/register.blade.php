426e900243178ec4733086ecad81d6cbc28bde2f
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-06 16:41:45 +0700

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,34 +2,56 @@
 
 @section ('content')
 <div id="auth-content">
+	<div class="register">
+		<div class="regcon">
+			<h1 class="reghead">สมัครสมาชิก</h1>
+			<h3 class="regsubh">Hippo Car Rent</h3>
 
+			<form method="POST" action="/register">
 
-<div class="register">
-<div class="regcon">
-	<h1 class="reghead">สมัครสมาชิก</h1>
-    <h3 class="regsubh">Hippo Car Rent</h3>
-    <p class="regcate">ชื่อจริง</p>
-    <input name="" type="text" class="regform"/>
-    <p class="regcate">นามสกุล</p>
-    <input name="" type="text" class="regform"/>
-    <p class="regcate">อีเมล</p>
-    <input name="" type="text" class="regform"/>
-    <p class="regcate">รหัสผ่าน</p>
-    <input name="" type="text" class="regform"/>
-    <p class="regcate">ยืนยันรหัสผ่าน</p>
-    <input name="" type="text" class="regform"/>
-    <p class="regcate">เบอร์โทรศัพท์</p>
-    <input name="" type="text" class="regform"/>
-    <div class="regcheck">
-    	<input name="" type="checkbox" value="" />
-        <p>ข้าพเจ้ายอมรับเงื่อนไขของ Hippo Car Rent</p>
-    </div>
-    <input name="" type="button" value="สมัครสมาชิก" class="regregist"/>
-</div>
-<img alt="" src="images/01-register_27.jpg" class="regfacebook" />
+				{{ csrf_field() }}
 
-<div class="clear"></div>
-</div>
+				<div class="form-group">
+						<label class="regcate" for="firstname">ชื่อจริง</label>
+						<input type="text" class="form-control regform" id="firstname" name="firstname" />
+				</div>
+
+				<div class="form-group">
+						<label class="regcate" for="lastname">นามสกุล</label>
+						<input type="text" class="form-control regform" id="lastname" name="lastname" />
+				</div>
+
+				<div class="form-group">
+						<label class="regcate" for="email">อีเมล</label>
+						<input type="email" class="form-control regform" id="email" name="email" />
+				</div>
+
+				<div class="form-group">
+						<label class="regcate" for="password">รหัสผ่าน</label>
+						<input type="password" class="form-control regform" id="password" name="password" />
+				</div>
+
+				<div class="form-group">
+						<label class="regcate" for="password_confirmation">ยืนยันรหัสผ่าน</label>
+						<input type="password" class="form-control regform" id="password_confirmation" name="password_confirmation" />
+				</div>
+
+				<div class="form-group">
+						<label class="regcate" for="telephone">เบอร์โทรศัพท์</label>
+						<input type="password" class="form-control regform" id="telephone" name="telephone" />
+				</div>
+
+				<div class="regcheck">
+					<input name="" type="checkbox" value="" />
+					<p>ข้าพเจ้ายอมรับเงื่อนไขของ Hippo Car Rent</p>
+				</div>
+				<input name="" type="button" value="สมัครสมาชิก" class="regregist"/>
+			</form>
+		</div>
+		<img alt="" src="images/01-register_27.jpg" class="regfacebook" />
+
+		<div class="clear"></div>
+	</div>
 
 </div>
 @endsection
