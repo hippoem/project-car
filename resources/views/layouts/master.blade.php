f7304acc317cd72e452f31ddcfda623f975baacb
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-07 09:08:07 +0700

    Title

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Untitled Document</title>
+	<title> Hippo Car Rent</title>
 	<script src="main.js" type="text/javascript"></script>
 	<link href="style.css" rel="stylesheet" type="text/css" />
 	<link href="phone.css" rel="stylesheet" type="text/css" />
