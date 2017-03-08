27cb70907257b5403aedd02ec203714a0450dad7
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-08 15:24:59 +0700

    migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('firstname');
-            $table->string('lastname');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('mobile');
+            $table->enum('sex', ['male', 'female', 'other'])->nullable();
+            $table->date('birthday');
+            $table->text('address')->nullable();
+            $table->text('facebook')->nullable();
+            $table->string('otp',5)->nullable();
+            $table->text('about')->nullable();
+            $table->boolean('verify_mobile')->default('0');
+            $table->boolean('verify_email')->default('0');
+            $table->string('dr_license')->nullable();
+            $table->string('license_expire_date')->nullable();
+            $table->tinyInteger('status')->default('1');
+            $table->enum('type', ['renter', 'owner', 'partner'])->default('renter');
             $table->rememberToken();
             $table->timestamps();
         });
