6342018fb48a577a1a63cbe4485c5cf9099dcd74
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-03-15 16:48:29 +0700

    fix bug id

--- resources/views/announces/announce.blade.php
+++ resources/views/announces/announce.blade.php
@@ -1,5 +1,5 @@
 <div class="fcbbox">
-       <a target="_blank" href="{{ url('cars') }}/{{ $announce->id }}">
+       <a target="_blank" href="{{ url('cars') }}/{{ $announce->ann_id }}">
               <img alt="" src="{{ $announce->ann_photo }}" class="fcbcar" />
        </a>
        <p class="fcbtag">NEW</p>
@@ -8,7 +8,7 @@
        </a>
        <div class="fcbdetail">
               <div class="fcbleft">
-                     <a target="_blank" href="{{ url('cars') }}/{{ $announce->id }}"><h3>เช่ารถ {{ $announce->genes->brands->brand_name_eng }} {{ $announce->genes->gene_name_eng }}</h3></a>
+                     <a target="_blank" href="{{ url('cars') }}/{{ $announce->ann_id }}"><h3>เช่ารถ {{ $announce->genes->brands->brand_name_eng }} {{ $announce->genes->gene_name_eng }}</h3></a>
                      <div class="fcbrate">
                             <img alt="" src="images/03-car-rent-single_15.png" />
                             <img alt="" src="images/03-car-rent-single_15.png" />
@@ -17,7 +17,7 @@
                             <img alt="" src="images/03-car-rent-single_18.png" />
                      </div>
               </div>
-       <a target="_blank" href="{{ url('cars') }}/{{ $announce->id }}">
+       <a target="_blank" href="{{ url('cars') }}/{{ $announce->ann_id }}">
               <p class="fcbprice">฿{{ number_format($announce->price)}}<span>/วัน</span></p>
        </a>
        </div>
